0ee9b9f76d53a19ef15e1eb42b83ffd42fabc1e0
Paradox0312 <nils@oger.bio>
2025-12-03 20:35:03 +0100

    J3

--- 2025/J3.ml
+++ 2025/J3.ml
@@ -2,20 +2,7 @@
 let list_of_enter (s:string) : string list =
   String.split_on_char '\n' s
 
-let max_jont (s:string) : int =
-  let n_max = ref 0 in
-  for i = 0 to String.length s - 1 do
-    for j = i+1 to String.length s - 1 do
-      let n = (int_of_char s.[i] * 10) + int_of_char s.[j] - 528 in
-      if n > !n_max
-      then n_max := n
-    done
-  done;
-  !n_max
-
-let max_jont_list (l:string list) : int = List.fold_left (+) 0 (List.map max_jont l)
-
-let max_jont2 (s:string) : int =
+let max_sous_mots (len:int)(s:string) : int =
   let rec max_aux (i:int)(n:int) : string =
     if n = 0
     then ""
@@ -25,6 +12,6 @@ let max_jont2 (s:string) : int =
       then j_max := j
     done;
     (String.make 1 s.[!j_max]) ^ (max_aux (!j_max+1) (n-1))
-  in int_of_string (max_aux 0 12)
+  in int_of_string (max_aux 0 len)
 
-let max_jont_list2 (l:string list) : int = List.fold_left (+) 0 (List.map max_jont2 l)
+let sum_max_sous_mots (len:int)(l:string list) : int = List.fold_left (+) 0 (List.map (max_sous_mots len) l)
